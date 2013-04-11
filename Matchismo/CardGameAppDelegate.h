936b8e86408b228e909edc6feee161834ef9678c
Vaibhav Patel <patel.vaibhav@gmail.com>
2013-04-11 16:01:02 -0700

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by vaibhav patel on 4/11/13.
+//  Copyright (c) 2013 Margev Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by vaibhav patel on 4/11/13.
+//  Copyright (c) 2013 Margev Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
